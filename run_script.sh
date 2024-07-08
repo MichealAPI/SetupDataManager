4565fa536bf419dd0e5475c067d7d5c6a00d965e
Michele <michele.cippone01@gmail.com>
2024-07-08 16:18:36 +0200

    Fixed and tested run_script.sh structure

--- run_script.sh
+++ run_script.sh
@@ -3,17 +3,25 @@
 # Define the name of the virtual environment directory
 ENV_DIR="myenv"
 
-# Activate the virtual environment
-if [ -f "$ENV_DIR/Scripts/activate" ]; then
-    # Windows
-    source $ENV_DIR/Scripts/activate
-else
-    # macOS/Linux
-    source $ENV_DIR/bin/activate
-fi
+# Function to run the Python script in the virtual environment
+run_in_virtualenv() {
+    # Activate the virtual environment
+    if [ -f "$ENV_DIR/Scripts/activate" ]; then
+        # Windows
+        source $ENV_DIR/Scripts/activate
+    else
+        # macOS/Linux
+        source $ENV_DIR/bin/activate
+    fi
 
-# Run the Python script
-python main.py
+    # Run the Python script
+    python main.py
 
-# Deactivate the virtual environment
-deactivate
+    # Deactivate the virtual environment
+    deactivate
+}
+
+# Run the function in a sub-shell
+(
+    run_in_virtualenv
+)
